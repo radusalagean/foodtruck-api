3b030ce6ee2c539d22c0c0ab8f9424a158e4fd87
rsalagean <radusalagean@hotmail.com>
2019-04-12 21:37:16 +0300

    Added reviews

--- src/controller/foodtruck.js
+++ src/controller/foodtruck.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { Router } from 'express';
 import Foodtruck from '../model/foodtruck';
+import Review from '../model/review';
 
 export default({ config, db }) => {
   let api = Router();
@@ -9,6 +10,9 @@ export default({ config, db }) => {
   api.post('/add', (req, res) => {
     let newFoodtruck = new Foodtruck();
     newFoodtruck.name = req.body.name;
+    newFoodtruck.foodtype = req.body.foodtype;
+    newFoodtruck.avgcost = req.body.avgcost;
+    newFoodtruck.geometry.coordinates = req.body.geometry.coordinates;
 
     newFoodtruck.save(err => {
       if (err) {
@@ -63,6 +67,56 @@ export default({ config, db }) => {
         res.send(err);
       }
       res.json({ message: "Foodtruck successfully removed" });
+    });
+  });
+
+  // Add review for a specific foodtruck id
+  // '/v1/foodtruck/reviews/add/:id'
+  api.post('/reviews/add/:id', (req, res) => {
+    Foodtruck.findById(req.params.id, (err, foodtruck) => {
+      if (err) {
+        res.send(err);
+      }
+      let newReview = new Review();
+      newReview.title = req.body.title;
+      newReview.text = req.body.text;
+      newReview.foodtruck = foodtruck._id;
+      newReview.save((err, review) => {
+        if (err) {
+          res.send(err);
+        }
+        foodtruck.reviews.push(newReview);
+        foodtruck.save(err => {
+          if (err) {
+            res.send(err);
+          }
+          res.json({ message: 'Review added successfully' });
+        });
+      });
+    });
+  });
+
+  // Get reviews for a specific foodtruck id
+  // '/v1/foodtruck/reviews/:id'
+  api.get('/reviews/:id', (req, res) => {
+    Review.find({foodtruck: req.params.id}, (err, review) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(review);
+    });
+  });
+
+  // Get food truck which serve a specific food type
+  // '/v1/foodtruck/foodtype/:foodtype'
+  api.get('/foodtype/:foodtype', (req, res) => {
+    Foodtruck.find({foodtype: {
+      $regex: new RegExp(req.params.foodtype.toLowerCase(), "i")
+    }}, (err, foodtrucks) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(foodtrucks);
     });
   });
 
--- src/model/foodtruck.js
+++ src/model/foodtruck.js
@@ -1,8 +1,28 @@
 import mongoose from 'mongoose';
+import Review from './review';
 let Schema = mongoose.Schema;
 
-let foodtruckSchema = new Schema({
-  name: String
+let FoodtruckSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  foodtype: {
+    type: String,
+    required: true
+  },
+  avgcost: Number,
+  geometry: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [Number]
+  },
+  reviews: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Review'
+  }]
 });
 
-module.exports = mongoose.model('Foodtruck', foodtruckSchema);
+module.exports = mongoose.model('Foodtruck', FoodtruckSchema);
