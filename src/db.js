8ae00a837c04eaafcf4e597b22ad8bbfa5755e66
rsalagean <radusalagean@hotmail.com>
2019-04-15 21:24:10 +0300

    mongoose promise fix

--- src/db.js
+++ src/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import config from './config';
 
 export default callback => {
-  mongoose.Promise = global.promise;
   let db = mongoose.connect(config.mongoUrl);
   callback(db);
 }
